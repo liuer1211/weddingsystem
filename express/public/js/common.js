9452ff3a0c84e0eb6652f9e4b8b4d9d1f43ad51c
wdd <296151852@qq.com>
2022-10-16 22:15:03 +0800

    1

--- express/public/js/common.js
+++ express/public/js/common.js
@@ -1,1 +1,12 @@
+// 监听滚动条变化
+$(window).scroll(function(){
+  let scrollHeight = document.documentElement.scrollTop||document.body.scrollTop;
+  let nodeId = document.getElementById('head')
+  if(scrollHeight>80){
+    nodeId.setAttribute('class', 'head scale')
+  } else {
+    nodeId.setAttribute('class', 'head')
+  }
+})
+
 // 头部菜单点击切换
