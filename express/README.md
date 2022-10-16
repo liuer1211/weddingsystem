bb25013e4f17bc40bb0e7a8d11d3f03aa6e26529
wdd <296151852@qq.com>
2022-10-16 14:52:42 +0800

    1

--- express/README.md
+++ express/README.md
@@ -5,4 +5,22 @@
   node+express
 
 # 服务端渲染
-  
+  npm init
+  npm i express
+
+  创建app.js
+
+  npm i errorhandler
+
+# 路由
+  加入路由
+  拆分页面 头 身 尾
+  模板引擎拼接
+
+# 模板引擎
+  npm i art-template express-art-template
+
+# 静态资源
+  配置资源开放
+  加入ionicons图标库
+  npm install ionicons@2
--- express/app.js
+++ express/app.js
@@ -1,10 +1,33 @@
-const express = require('express')
+const errorHandler = require('errorhandler');
+const express = require('express');
+const path = require('path');
+// const morgan = require('morgan') // 日志
+const router = require('./router')
 
 const app = express();
 
-app.get('/',(req,res)=>{
-  res.send('hello')
+// 资源开放
+app.use('/public',express.static(path.join(__dirname,'./public')))
+app.use('/node_modules',express.static(path.join(__dirname,'./node_modules')))
+
+// 模板引擎
+app.engine('html',require('express-art-template'))
+app.set('view options',{
+  debug: process.env.NODE_ENV !== 'production'
 })
+app.set('views', path.join(__dirname,'views')) // 模板文件存储位置
+app.set('view engine','html') // 可以省略模板文件后缀名
+
+// app.use(morgan('dev'))
+app.use(router)
+app.use(express.json())  // json
+app.use(express.urlencoded({ // form表单
+  extended:true
+}))
+
+if(process.env.NODE_ENV === 'development'){
+  app.use(errorHandler())
+}
 
 app.listen(3000,()=>{
   console.log('3000端口')
